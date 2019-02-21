77cbb22f23f2db1a8df7290bea64dc0d0971de7f
mir <mir@mbp15tb01.local>
2019-02-21 11:47:17 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,97 @@
+(function () {
+  const {
+    random,
+    abs
+  } = Math;
+
+  const inputArray = [];
+
+  const $inputLength = document.getElementById('inputLength');
+  const $inputArray = document.getElementById('inputArray');
+  const $outputSetOne = document.getElementById('outputSetOne');
+  const $outputSetTwo = document.getElementById('outputSetTwo');
+  const $outputDiff = document.getElementById('outputDiff');
+  const $more = document.getElementById('more');
+
+  function randomArray(size = 6, weight = 10) {
+    return [...Array(size)].map(
+      () => random() * weight ^ 0
+    );
+  }
+
+  function descendingSort() {
+    return this.length ? this.sort(
+      (a, b) => b - a
+    ) : [];
+  }
+
+  function sum() {
+    return this.reduce(
+      (a, b) => a + b, 0
+    );
+  }
+
+  function partition() {
+    const subsetOne = [];
+    const subsetTwo = [];
+    let sumOne = 0;
+    let sumTwo = 0;
+
+    for (let value of this) {
+      sumOne = sum.call(subsetOne);
+      sumTwo = sum.call(subsetTwo);
+      if (sumOne < sumTwo) {
+        subsetOne.push(value);
+      } else {
+        subsetTwo.push(value);
+      }
+    }
+
+    const difference = abs(sumOne - sumTwo);
+
+    return {
+      subsetOne,
+      subsetTwo,
+      sumOne,
+      sumTwo,
+      difference
+    };
+  }
+
+  function renderStart() {
+    $inputArray.innerText = `[${this.toString()}]`;
+    $inputLength.innerText = `${this.length.toString()}`;
+  }
+
+  function renderOutput() {
+    const {
+      subsetOne,
+      subsetTwo,
+      sumOne,
+      sumTwo,
+      difference
+    } = this;
+
+    const moreText = `(The sum of the first subarray is ${sumOne}, the sum of the second subarray is ${sumTwo}, the difference is ${difference}.)`;
+
+    $outputSetOne.innerText = `[${subsetOne.toString()}]`;
+    $outputSetTwo.innerText = `[${subsetTwo.toString()}]`;
+    $outputDiff.innerText = `${difference.toString()}`;
+    $more.innerText = moreText;
+  }
+
+  function init() {
+    const startArray = inputArray.length ? inputArray : randomArray(7, 100);
+    renderStart.call(startArray);
+    // Sort in descending order
+    descendingSort.call(startArray);
+    const outputData = partition.call(startArray);
+    document.getElementById('getSubsets').addEventListener('click', () => renderOutput.call(outputData));
+  }
+
+  window.onload = function () {
+    init();
+  };
+
+})();
+
